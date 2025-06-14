98906b9415fea0124ce543884b61e9953ef35b7d
Hviliy <danilakopylov603@gmail.com>
2025-06-14 19:49:30 +0800

    - исправлена функция удаления профиля

--- backend/app/routers/profile.py
+++ backend/app/routers/profile.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, status
 from datetime import datetime, timezone
 
 from sqlalchemy import literal
